cb151e21fddd91c44651393873b4c129c6c2c316
unknown <tomneutens@gmail.com>
2015-03-11 17:48:59 +0100

    initial commit

---
+++ OpendeurGuide/public_html/js/scanner.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+
+    var menu_open = false;
+
+    $('#reader').html5_qrcode(function (data) {
+        if (menu_open){
+            alert(data);
+            scrollToAnchor(data);
+            animateMenu();
+        }
+    },function (error) {
+                //show read errors 
+    }, function (videoError) {
+        //the video stream could be opened
+    }
+    );
+
+    $("#scan_button").click(function () {
+        animateMenu();
+    });
+
+    function animateMenu() {
+        if (!menu_open) {
+            menu_open = true;
+            $(".scan_menu").animate(
+                    {
+                        top: '+=300'
+                    });
+        } else {
+            menu_open = false;
+            $(".scan_menu").animate(
+                    {
+                        top: '-=300'
+                    });
+        }
+    }
+
+    function scrollToAnchor(aid) {
+        var aTag = $("#" + aid);
+        $('html,body').animate({scrollTop: aTag.offset().top}, 'slow');
+    }
+
+};
