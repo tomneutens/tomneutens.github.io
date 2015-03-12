9f9c839e9ecb588c3c687b06839608421d4ae009
unknown <tomneutens@gmail.com>
2015-03-12 15:07:57 +0100

    select first source

--- OpendeurGuide/public_html/js/scanner.js
+++ OpendeurGuide/public_html/js/scanner.js
@@ -1,20 +1,45 @@
 window.onload = function () {
 
+
+    MediaStreamTrack.getSources(function (sourceInfos) {
+        var videoSources = [];
+
+        for (var i = 0; i != sourceInfos.length; ++i) {
+            var sourceInfo = sourceInfos[i];
+            if (sourceInfo.kind === 'video') {
+                console.log("found video source");
+                console.log(sourceInfo);
+                videoSources.push(sourceInfo.id);
+            } else {
+                console.log('Some other kind of source: ', sourceInfo);
+            }
+        }
+
+        selectSource(videoSources);
+    });
+
+    function selectSource(videoSources) {
+        for (var i = 0; i < videoSources.length; ++i) {
+            console.log(videoSources[i]);
+        }
+        $('#reader').html5_qrcode(function (data) {
+            if (menu_open) {
+                alert(data);
+                scrollToAnchor(data);
+                animateMenu();
+            }
+        }, function (error) {
+            // alert(error);
+        }, function (videoError) {
+            //the video stream could be opened
+            alert("video stream could be opened");
+        }, videoSources[0]
+        );
+    }
+
     var menu_open = false;
 
-    $('#reader').html5_qrcode(function (data) {
-        if (menu_open){
-            alert(data);
-            scrollToAnchor(data);
-            animateMenu();
-        }
-    },function (error) {
-        // alert(error);
-    }, function (videoError) {
-        //the video stream could be opened
-        alert("video stream could be opened");
-    }
-    );
+
 
     $("#scan_button").click(function () {
         animateMenu();
